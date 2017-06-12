633bd59092e39108b01e7f04a159bdab03ec9dd0
tao wei <stevenlt4@hotmail.com>
2017-06-12 19:47:54 +0800

    Create app.js

---
+++ codeAcademy/jQueryProjects/threadly/app.js
@@ -0,0 +1,13 @@
+var main = function() {
+  $('form').submit(function() {
+    var comment = $('#comment').val();
+    if(comment !== "") {
+      var html = $('<li>').text(comment);
+      $(html).prependTo('.comments');
+      $('#comment').val("");      
+    }
+    return false;
+  });
+};
+
+$(document).ready(main);
