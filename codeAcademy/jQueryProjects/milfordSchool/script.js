4ce2db5fb643720feef8ae9a43dbde327a80019a
tao wei <stevenlt4@hotmail.com>
2017-06-17 12:24:03 +0800

    Create script.js

---
+++ codeAcademy/jQueryProjects/milfordSchool/script.js
@@ -0,0 +1,26 @@
+var main = function() {
+  
+  $('img').click(function(){
+    $('.dropdown-menu').toggle();
+  });
+  
+ $('form').submit(function(){
+  
+   
+ 
+  
+  var email = $('#email').val();
+  var password = $('#password').val();
+  if(email === ''){
+    $('.email-error').text('Please enter your email.');
+  }
+  
+  if(password === ''){
+    $('.password-error').text('Please enter your password.');
+  }
+    return false;
+  
+  });
+};
+
+$(document).ready(main);
